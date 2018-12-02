fb92e8c8d4c451188bba5187e1808cb22bf59f19
axel <axelcazares012@gmail.com>
2018-12-01 20:30:47 -0700

    primer commit

---
+++ app/src/main/java/com/example/axelc/gamelibrary/Critica.java
@@ -0,0 +1,13 @@
+package com.example.axelc.gamelibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Critica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_critica);
+    }
+}
